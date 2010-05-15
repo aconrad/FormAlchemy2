4276d9fedc1bde6d9cd2e8c18c8c7b1931153f29
Alexandre Conrad <alexandre.conrad@gmail.com>
2010-05-15 20:23:29 +0200

    init renderer to None and raise NoRendererError if render() is called

--- formalchemy2/fields.py
+++ formalchemy2/fields.py
@@ -1,8 +1,13 @@
 #coding: utf-8
+from formalchemy2.exceptions import *
 
 class Field(object):
     def __init__(self, id):
         self.id = id
+        self.renderer = None
 
     def render(self):
-        raise NotImplementedError
+        """Call and return renderer's data."""
+        if self.renderer is None:
+            raise NoRendererError
+        return self.renderer.render()
--- test/test_fields.py
+++ test/test_fields.py
@@ -4,6 +4,8 @@
 from unittest import TestCase
 
 from formalchemy2.fields import Field
+from formalchemy2.renderer import Renderer
+from formalchemy2.exceptions import *
 
 
 class TestField(TestCase):
@@ -14,4 +16,4 @@ class TestField(TestCase):
 
     def test_field_render(self):
         field = Field('name')
-        self.assertRaises(NotImplementedError, field.render)
+        self.assertRaises(NoRendererError, field.render)
