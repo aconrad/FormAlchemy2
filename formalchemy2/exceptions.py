abe036026c79608433b5967ff18c17bbffe4140f
Alexandre Conrad <alexandre.conrad@gmail.com>
2010-05-15 20:18:58 +0200

    add NoRendererError

--- formalchemy2/exceptions.py
+++ formalchemy2/exceptions.py
@@ -1,9 +1,12 @@
 #coding: utf-8
 
-__all__ = ['FormAlchemyError', 'RendererError']
+__all__ = ['FormAlchemyError', 'RendererError', 'NoRendererError']
 
 class FormAlchemyError(Exception):
     """Generic error class."""
 
 class RendererError(FormAlchemyError):
     """Generic renderer error."""
+
+class NoRendererError(RendererError):
+    """Raised when no renderer is set."""
