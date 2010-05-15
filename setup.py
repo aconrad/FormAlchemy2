079f9f43aaaed3eaf01652963ee2f1a980d1ce25
Alexandre Conrad <alexandre.conrad@gmail.com>
2010-05-15 18:18:15 +0200

    hello FormAlchemy2

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='FormAlchemy2',
+    version='0.1dev',
+    description='FormAlchemy2 generates forms, validates and stores input data to your favorite backend.',
+    author='Alexandre Conrad',
+    author_email='alexandre conrad gmail com',
+    url='http://formalchemy.org',
+    packages=['formalchemy2',],
+    license='MIT License',
+    long_description=open('README').read(),
+)
