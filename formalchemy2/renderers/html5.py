91a3444a0f19848e7d3591cf0021ac387d46bf2d
Alexandre Conrad <alexandre.conrad@gmail.com>
2010-05-16 20:28:13 +0200

    start html5 TextInput

---
+++ formalchemy2/renderers/html5.py
@@ -0,0 +1,22 @@
+#coding: utf-8
+from mako.template import Template
+
+from formalchemy2.renderers import Renderer
+
+
+INPUT_TEXT = u"""\
+% if field.label:
+<label for=${field.id}>${field.label}</label>
+% endif
+<input id=${field.id} type=text value=${field.value or ""} />
+"""
+
+
+class TextInput(Renderer):
+
+    group = u"html5"
+    name = u"text_input"
+
+    def render(self, field):
+        template = Template(INPUT_TEXT)
+        return template.render_unicode(field=field)
---
+++ test/test_renderer_html5.py
@@ -0,0 +1,17 @@
+#coding: utf-8
+
+from unittest import TestCase
+
+from formalchemy2.fields import Field
+from formalchemy2.renderers.html5 import TextInput
+
+
+class TestRenderer(TestCase):
+
+    def test_field_render(self):
+        field = Field('id', label='foo')
+        renderer = TextInput()
+        out = renderer.render(field)
+        assert isinstance(out, unicode)
+        assert u"input" in out
+        assert u"label" in out
