477d74163a8566be1df366e2312f28d9eed27659
Alexandre Conrad <alexandre.conrad@gmail.com>
2010-05-19 09:49:58 +0200

    simplify rendering tests

--- test/test_renderer_html5.py
+++ test/test_renderer_html5.py
@@ -5,33 +5,49 @@ from unittest import TestCase
 from formalchemy2.fields import Field, FieldMultiChoice
 from formalchemy2.renderers.html5 import TextInput, Select
 
-# relative import
-import base_renderers
+
+class MixinRendererTest(object):
+
+    def test_render_unicode(self):
+        """Test that rendering output is unicode."""
+        raise NotImplementedError
+
+    def test_render_encoding(self):
+        """Test that rendering output is str."""
+        raise NotImplementedError
 
 
-class TestTextInputRenderer(TestCase, base_renderers.FieldRendererMixin):
+class TestTextInputRenderer(TestCase, MixinRendererTest):
 
     Renderer = TextInput
 
-    def test_field_render(self):
+    def test_render_unicode(self):
         field = Field('id', label='my field')
         renderer = self.Renderer()
         output = renderer.render(field)
-        assert u'label' in output
-        assert u'input' in output
+        assert isinstance(output, unicode)
+
+    def test_render_encoding(self):
+        field = Field('id', label='my field')
+        renderer = self.Renderer(encoding='utf-8')
+        output = renderer.render(field)
+        assert isinstance(output, str)
 
 
-class TestSelectRenderer(TestCase, base_renderers.FieldMultiChoiceRendererMixin):
+class TestSelectRenderer(TestCase, MixinRendererTest):
 
     Renderer = Select
 
-    def test_field_render(self):
-        menu = (
-            ('C6', '2 sushis, 6 california, 5 brochettes, riz'),
-            ('N', 'shirashi saumon'),
-        )
-        field = FieldMultiChoice('id', choices=menu, label='my menu')
+    def test_render_unicode(self):
+        choices = (('foo', 'Foo'), )
+        field = FieldMultiChoice('id', choices=choices, label='my choices')
         renderer = self.Renderer()
         output = renderer.render(field)
-        assert u'california' in output
-        assert u'my menu' in output
+        assert isinstance(output, unicode)
+
+    def test_render_encoding(self):
+        choices = (('foo', 'Foo'), )
+        field = FieldMultiChoice('id', choices=choices, label='my choices')
+        renderer = self.Renderer(encoding='utf-8')
+        output = renderer.render(field)
+        assert isinstance(output, str)
