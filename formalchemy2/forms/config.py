e54548bca0a9044abaab67e1c183d1a356f459ff
Alexandre Conrad <alexandre.conrad@gmail.com>
2010-05-23 13:53:57 +0200

    rename kwarg of ConfigForm 'section' to 'sections' and refactor ConfigForm tests

--- formalchemy2/forms/config.py
+++ formalchemy2/forms/config.py
@@ -13,14 +13,19 @@ class ConfigForm(Form):
     Arguments:
     config -- a ConfigParser instance
 
+    Keyword arguments:
+    sections -- an iterable of section names to be included
+
     """
-    def __init__(self, config, section=None, *args, **kwargs):
+    def __init__(self, config, sections=None, *args, **kwargs):
         super(ConfigForm, self).__init__(*args, **kwargs)
 
-        self.section = section
+        # Put all fields when no section is given
+        if sections is None:
+            sections = config.sections()
 
         # Populate fields from config options
-        for section in config.sections():
+        for section in sections:
             for option, value in config.items(section):
                 field_id = "%s-%s" % (section, option)
                 field = Field(field_id, label=option, value=value)
--- tests/test_form_config.py
+++ tests/test_form_config.py
@@ -8,22 +8,38 @@ from formalchemy2.forms.config import ConfigForm
 
 class TestConfigForm(TestCase):
 
+    @classmethod
+    def setUp(cls):
+        # Setup config
+        config = SafeConfigParser()
+        config.add_section('foo')
+        config.set('foo', 'foo_option1', 'foo_value1')
+        config.set('foo', 'foo_option2', 'foo_value2')
+        config.add_section('bar')
+        config.set('bar', 'bar_option1', 'bar_value1')
+        config.set('bar', 'bar_option2', 'bar_value2')
+
+        cls.config = config
+
+    @classmethod
+    def tearDown(cls):
+        del cls.config
+
     def test_init(self):
+        form = ConfigForm(self.config)
+        assert form.fields, 'No field found in form'
+        field = form.fields['foo-foo_option1']
+        assert field.id == 'foo-foo_option1'
+        assert field.label == 'foo_option1'
+        assert field.value == 'foo_value1'
+        assert form.fields['bar-bar_option1']
+
+    def test_init_empty_config(self):
         config = SafeConfigParser()
         form = ConfigForm(config)
         assert not form.fields
-        assert form.section is None
 
-    def test_fields_from_config(self):
-        # Setup config
-        config = SafeConfigParser()
-        config.add_section('section')
-        config.set('section', 'option', 'value')
-
-        # Generate form from config
-        form = ConfigForm(config)
-        assert form.fields, 'No field found in form'
-        field = form.fields['section-option']
-        assert field.id == 'section-option'
-        assert field.label == 'option'
-        assert field.value == 'value'
+    def test_fields_from_config_section(self):
+        form = ConfigForm(self.config, sections=['foo'])
+        assert form.fields['foo-foo_option1']
+        self.assertRaises(KeyError, form.fields.__getitem__, 'bar-bar_option1')
