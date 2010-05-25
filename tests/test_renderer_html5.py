f50bc1194b0e749ee6c50b538f64a147c3608118
Alexandre <alexandre.conrad@gmail.com>
2010-05-25 12:18:35 +0200

    make better testing of html5 renderer

--- tests/test_renderer_html5.py
+++ tests/test_renderer_html5.py
@@ -10,6 +10,26 @@ class TestTextInputRenderer(TestCase):
 
     Renderer = TextInput
 
+    def test_output(self):
+        renderer = self.Renderer()
+        field = Field('id', renderer=renderer)
+        output = field.render()
+        assert 'type=text' in output
+        assert 'id' in output
+        assert 'label' in output
+        assert 'name' in output
+        assert 'value' not in output
+
+    def test_output_with_value(self):
+        renderer = self.Renderer()
+        field = Field('id', value='foo', renderer=renderer)
+        output = field.render()
+        assert 'type=text' in output
+        assert 'id' in output
+        assert 'label' in output
+        assert 'name' in output
+        assert 'value="foo"' in output
+
 
 class TestSelectRenderer(TestCase):
 
@@ -21,9 +41,20 @@ class TestSelectRenderer(TestCase):
         field = Field('id', choices=choices, renderer=renderer)
         output = field.render()
         assert 'name' in output
-        assert 'select' in output
+        assert '<select' in output
         assert 'option' in output
         assert 'foo' in output
+        assert 'selected' not in output
+
+    def test_output_with_seclected(self):
+        renderer = self.Renderer()
+        choices = [('foo', 'Foo')]
+        field = Field('id', value='foo', choices=choices, renderer=renderer)
+        output = field.render()
+        assert 'name' in output
+        assert '<select' in output
+        assert 'option' in output
+        assert 'value="foo" selected' in output
 
 
 class TestHiddenInputRenderer(TestCase):
@@ -34,9 +65,14 @@ class TestHiddenInputRenderer(TestCase):
         renderer = self.Renderer()
         field = Field('id', renderer=renderer)
         output = field.render()
-        assert 'name' in output
         assert 'type=hidden' in output
+        assert 'name' in output
         assert 'value' not in output
+
+    def test_output_with_value(self):
+        renderer = self.Renderer()
         field = Field('id', value='PUT', renderer=renderer)
         output = field.render()
-        assert 'value' in output
+        assert 'type=hidden' in output
+        assert 'name' in output
+        assert 'value="PUT"' in output
