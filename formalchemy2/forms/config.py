72933d121587ef7597d5e031efb81c7bb807eef3
Alexandre <alexandre.conrad@gmail.com>
2010-06-07 18:05:05 +0200

    use an id (de)serializer for ConfigForm

--- formalchemy2/forms/config.py
+++ formalchemy2/forms/config.py
@@ -4,6 +4,22 @@ from formalchemy2.fields import Field
 from formalchemy2.forms import Form
 
 
+SECTION_OPTION_SEP = u"-"
+
+
+def serialize_id(section, option):
+    """Return a config's section and option serialized to some id."""
+    return SECTION_OPTION_SEP.join([section, option])
+
+
+def deserialize_id(id):
+    """Return a tuple (section, option) given an id serialized by
+    serialize_id().
+
+    """
+    return id.split(SECTION_OPTION_SEP, 1)
+
+
 class ConfigForm(Form):
     """The Config form.
 
@@ -28,12 +44,12 @@ class ConfigForm(Form):
         # Populate fields from config options
         for section in sections:
             for option, value in config.items(section):
-                field_id = "%s-%s" % (section, option)
+                field_id = serialize_id(section, option)
                 field = Field(field_id, label=option, value=value)
                 self.append(field)
 
     def sync(self, config):
         """Sync form's data to given config."""
         for field in self:
-            section, option = field.id.split('-', 1)
+            section, option = deserialize_id(field.id)
             config.set(section, option, str(field.value))
